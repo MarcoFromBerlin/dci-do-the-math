ea1b9cf1179f773c28829228a4750f3b09efde1f
Marco Vignotto <marcovignotto81@gmail.com>
2021-01-19 10:36:32 +0100

    zoom delegated

--- dist/scripts/animation.js
+++ dist/scripts/animation.js
@@ -4,7 +4,7 @@ const btnRotation = document.querySelectorAll(".btn-rotate");
 // console.log(btnRotation);
 const btnRotationBack = document.querySelectorAll(".btn-rotate-back");
 
-const btnCodeZoom = document.querySelector(".btn-code-zoom");
+const btnCodeZoom = document.querySelectorAll(".btn-code-zoom");
 
 const card = document.querySelector(".flip-card-inner");
 // console.log(card);
@@ -35,6 +35,8 @@ const rotate = (e) => {
 };
 
 const rotateBack = (e) => {
+  e.preventDefault();
+
   if (e.target.classList.contains("btn-rotate-back")) {
     console.log("rotate-back");
     setTimeout(() => {
@@ -46,9 +48,69 @@ const rotateBack = (e) => {
       "rotateY(360deg)";
   }
 };
+
+const zoomCode = (e) => {
+  e.preventDefault();
+  let isZoomed = false;
+  if (e.target.classList.contains("btn-code-zoom")) {
+    let heightCode = languageJava.offsetHeight + 130;
+    let widthCode = languageJava.offsetWidth; // 960
+    let sectionHeight = e.target.closest(".math-container").offsetHeight;
+
+    // let getWidthCode = codeBox.offsetWidth; // 614
+
+    if (!isZoomed) localStorage.setItem("oldWidthCode", codeBox.offsetWidth);
+    if (!isZoomed) localStorage.setItem("oldHeightCode", codeBox.offsetHeight);
+    if (!isZoomed) localStorage.setItem("oldHeightSection", sectionHeight);
+
+    let oldWidthCode = localStorage.getItem("oldWidthCode");
+    let oldHeightCode = localStorage.getItem("oldHeightCode");
+    let oldHeightSection = localStorage.getItem("oldHeightSection");
+
+    let paddingVert = 250;
+    let paddingHorr = 80;
+
+    // add padding if its bigger
+    let calcOptimalWidth =
+      mathContainer.clientWidth >= widthCode + paddingHorr * 2
+        ? widthCode + paddingHorr * 2
+        : widthCode;
+
+    if (!isZoomed) {
+      codeBox.style.transition = "all 0.3s";
+
+      codeBox.style.marginLeft = `-${(calcOptimalWidth - oldWidthCode) / 2}px`;
+      codeBox.style.width = `${calcOptimalWidth}px`;
+      codeBox.style.height = `${heightCode}px`;
+
+      languageJava.style.height = `${heightCode}px`;
+
+      languageJavaContainer.classList.remove("set-max-height");
+
+      mathContainer.style.transition = "all 0.2s";
+      mathContainer.style.height = `${heightCode + paddingVert}px`;
+
+      isZoomed = true;
+    } else if (isZoomed) {
+      codeBox.style.transition = "all 0.2s";
+      codeBox.style.width = `${oldWidthCode}px`;
+      codeBox.style.marginLeft = 0;
+      codeBox.style.height = `${oldHeightCode}px`;
+
+      mathContainer.style.height = `${oldHeightSection}px`;
+
+      languageJavaContainer.classList.add("set-max-height");
+
+      isZoomed = false;
+    }
+  }
+  console.log("isZoomed", isZoomed);
+};
+
 /// event listeners
 document.body.addEventListener("click", rotate);
 document.body.addEventListener("click", rotateBack);
+document.body.addEventListener("click", zoomCode);
 
 btnRotation.forEach((element) => {
   element.addEventListener("click", rotate);
@@ -58,70 +120,8 @@ btnRotationBack.forEach((element) => {
   element.addEventListener("click", rotateBack);
 });
 
-// END EVENT LISTENERS
-
-let isZoomed = false;
-
-btnCodeZoom.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  let heightCode = languageJava.offsetHeight + 130;
-  let widthCode = languageJava.offsetWidth; // 960
-  let sectionHeight = e.target.closest(".math-container").offsetHeight;
-
-  // let getWidthCode = codeBox.offsetWidth; // 614
-
-  if (!isZoomed) localStorage.setItem("oldWidthCode", codeBox.offsetWidth);
-  if (!isZoomed) localStorage.setItem("oldHeightCode", codeBox.offsetHeight);
-  if (!isZoomed) localStorage.setItem("oldHeightSection", sectionHeight);
-
-  // console.log(codeBox.offsetHeight);
-
-  // console.log("oldHeightSection", localStorage.getItem("oldHeightSection"));
-
-  let oldWidthCode = localStorage.getItem("oldWidthCode");
-  let oldHeightCode = localStorage.getItem("oldHeightCode");
-  let oldHeightSection = localStorage.getItem("oldHeightSection");
-
-  let paddingVert = 250;
-  let paddingHorr = 80;
-
-  // add padding if its bigger
-  let calcOptimalWidth =
-    mathContainer.clientWidth >= widthCode + paddingHorr * 2
-      ? widthCode + paddingHorr * 2
-      : widthCode;
-
-  if (!isZoomed) {
-    codeBox.style.transition = "all 0.3s";
-
-    codeBox.style.marginLeft = `-${(calcOptimalWidth - oldWidthCode) / 2}px`;
-    codeBox.style.width = `${calcOptimalWidth}px`;
-    codeBox.style.height = `${heightCode}px`;
-
-    languageJava.style.height = `${heightCode}px`;
-
-    languageJavaContainer.classList.remove("set-max-height");
-
-    mathContainer.style.transition = "all 0.2s";
-    mathContainer.style.height = `${heightCode + paddingVert}px`;
-
-    isZoomed = true;
-  } else {
-    console.log(e.target.parentElement);
-    console.log(e.target.closest(".math-container").offsetHeight);
-
-    codeBox.style.transition = "all 0.3s";
-    codeBox.style.width = `${oldWidthCode}px`;
-    codeBox.style.marginLeft = 0;
-    codeBox.style.height = `${oldHeightCode}px`;
-
-    // languageJava.style.height = `${oldHeightSection}px`;
-
-    mathContainer.style.height = `${oldHeightSection}px`;
-
-    languageJavaContainer.classList.add("set-max-height");
-
-    isZoomed = false;
-  }
+btnCodeZoom.forEach((element) => {
+  element.addEventListener("click", zoomCode);
 });
+
+// END EVENT LISTENERS
